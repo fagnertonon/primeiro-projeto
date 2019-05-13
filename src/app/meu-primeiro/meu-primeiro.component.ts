b4c22232d96c32285c10d2ebd022c00288e6122a
fagner tonon <fagnertonon@hotmail.com>
2019-05-12 21:27:34 -0300

    aula 4

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+import { template } from '@angular/core/src/render3';
+
+
+@Component(
+    {
+        selector: 'meu-primeiro-component',
+        template: `
+        <P>Meu primeiro component com Angular Funcina<!p>
+                  `                 
+
+    }
+)
+export class MeuPrimeiroComponent {
+
+}
